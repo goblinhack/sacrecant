b65302384031d0c18a08a587ef5c7e7eda554c4e
Goblin Hack <goblinhack@gmail.com>
2026-09-24 12:39:54 +0100

    escape from spell targetting

--- src/game_events.cpp
+++ src/game_events.cpp
@@ -1227,5 +1227,34 @@ static auto game_event_abort(Gamep g) -> bool
           }
       }
   }
+
+  switch (game_state(g)) {
+    case STATE_COLLECT_MENU :        [[fallthrough]];
+    case STATE_DEAD_MENU :           [[fallthrough]];
+    case STATE_GENERATED :           [[fallthrough]];
+    case STATE_GENERATING :          [[fallthrough]];
+    case STATE_INIT :                [[fallthrough]];
+    case STATE_INVENTORY_MENU :      [[fallthrough]];
+    case STATE_ITEM_MENU :           [[fallthrough]];
+    case STATE_KEYBOARD_MENU :       [[fallthrough]];
+    case STATE_LEVEL_SELECT_MENU :   [[fallthrough]];
+    case STATE_PLAYER_SELECT_MENU :  [[fallthrough]];
+    case STATE_SPELL_LEARN_MENU :    [[fallthrough]];
+    case STATE_SPELLBOOK_MENU :      [[fallthrough]];
+    case STATE_LOAD_MENU :           [[fallthrough]];
+    case STATE_LOADED :              [[fallthrough]];
+    case STATE_MAIN_MENU :           [[fallthrough]];
+    case STATE_MOVE_WARNING_MENU :   [[fallthrough]];
+    case STATE_QUIT_MENU :           [[fallthrough]];
+    case STATE_QUITTING :            [[fallthrough]];
+    case STATE_SAVE_MENU :           [[fallthrough]];
+    case STATE_GAME_OVER_MENU :      [[fallthrough]];
+    case STATE_THROW_MENU :          [[fallthrough]];
+    case STATE_PLAYING :             [[fallthrough]];
+    case STATE_CHOOSE_THROW_TARGET : [[fallthrough]];
+    case STATE_CHOOSE_SPELL_TARGET : game_state_reset(g, "pressed escape"); break;
+    case GAME_STATE_ENUM_MAX :       DBG("game mouse down, ignore, not playing"); return false;
+  }
+
   return false;
 }
